cdee9640e02cd84803f876afe25c7e8310aa64ae
Esha Massand <ubemas001@mail.bbk.ac.uk>
2015-02-11 20:52:18 +0000

    Added gitignore

--- src/connect4/scala/TreeNode.scala
+++ src/connect4/scala/TreeNode.scala
@@ -1,7 +1,8 @@
 package connect4.scala
 
 /**
- * Created by liliya on 09/02/2015.
+ * 
+ * Created by liliya & Esha on 09/02/2015.
  */
 case class TreeNode(private var left: TreeNode, v: Int, private var right: TreeNode) {
   private var value: Int = v
